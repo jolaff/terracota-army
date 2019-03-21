ded2ae1c70e30486e6b3945e4714298926511abd
jolaff <joel.landgraf@gmail.com>
2019-03-21 05:33:35 -0300

    First commit with very basic movement

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float velocity;
+
+    private CharacterController characterController;
+
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement() {
+        float horInput = Input.GetAxis(horizontalInputName) * velocity;
+        float vertInput = Input.GetAxis(verticalInputName) * velocity;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horInput;
+
+        characterController.SimpleMove(forwardMovement + rightMovement);
+    }
+
+}
