2a9ce92856022cb204561fbb139afed80c7cfe91
jolaff <joel.landgraf@gmail.com>
2019-03-23 23:32:55 -0300

    player movement refab

---
+++ Assets/Scripts/Player/CameraBob.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[Serializable]
+public class CameraBob {
+    // Variables declaration.
+    [SerializeField] private float HorizontalBobRange;
+    [SerializeField] private float VerticalBobRange;
+    [SerializeField] private float VerticalToHorizontalRatio;
+    [SerializeField] private float BobDuration;
+    [SerializeField] private float BobAmount;
+    public AnimationCurve Bobcurve = new AnimationCurve(new Keyframe(0f, 0f),
+        new Keyframe(0.5f, 1f),
+        new Keyframe(1f, 0f),
+        new Keyframe(1.5f, -1f),
+        new Keyframe(2f, 0f));
+    private float cyclePositionX;
+    private float cyclePositionY;
+    private float bobBaseInterval;
+    private Vector3 originalCameraPosition;
+    private float t_Time;
+    private float offSet = 0.0f;
+    // End of variables declaration.
+
+    // Camera bobbing initialization.
+    public void Init(Camera camera, float bobBaseInt) {
+        bobBaseInterval = bobBaseInt;
+        originalCameraPosition = camera.transform.localPosition;
+        t_Time = Bobcurve[Bobcurve.length - 1].time;
+    }
+
+    public float OffSet() => offSet;
+
+    // Head Bobbing method according to speed (walk/run).
+    public Vector3 DoHeadBob(float speed) {
+        float xPos = originalCameraPosition.x + (Bobcurve.Evaluate(cyclePositionX) * HorizontalBobRange);
+        float yPos = originalCameraPosition.y + (Bobcurve.Evaluate(cyclePositionY) * VerticalBobRange);
+
+        cyclePositionX += (speed * Time.deltaTime) / bobBaseInterval;
+        cyclePositionY += ((speed * Time.deltaTime) / bobBaseInterval) * VerticalToHorizontalRatio;
+
+        if (cyclePositionX > t_Time) cyclePositionX = cyclePositionX - t_Time;
+        if (cyclePositionY > t_Time) cyclePositionY = cyclePositionY - t_Time;
+
+        return new Vector3(xPos, yPos, 0f);
+    }
+
+    public IEnumerator DoJumpBob() {
+        float t = 0f;
+        while(t<BobDuration) {
+            offSet = Mathf.Lerp(BobAmount, 0f, t/BobDuration);
+            t+=Time.deltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        offSet = 0f;
+    }
+
+}
---
+++ Assets/Scripts/Player/MouseCamera.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class MouseCamera {
+
+    // Variables declaration.
+    [SerializeField] private string xMouseInputName;
+    [SerializeField] private string yMouseInputName;
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private float smoothTime;
+
+    private Quaternion playerTargetRotation;
+    private Quaternion cameraTargetRotation;
+    // End of variables declaration.
+
+    // MouseCamera initialization.
+    public void Init(Transform player, Transform camera) {
+        playerTargetRotation = player.localRotation;
+        cameraTargetRotation = camera.localRotation;
+    }
+
+    // Rotates the camera on the X Axis and the player on the Y Axis.
+    public void CameraRotation(Transform player, Transform camera) {
+        float yRotation = Input.GetAxis(xMouseInputName) * mouseSensitivity;
+        float xRotation = Input.GetAxis(yMouseInputName) * mouseSensitivity;
+
+        playerTargetRotation *= Quaternion.Euler(0f, yRotation, 0f);
+        cameraTargetRotation *= Quaternion.Euler(-xRotation, 0f, 0f);
+
+        cameraTargetRotation = ClampXRotation(cameraTargetRotation); // Clamps the camera rotation on the X Axis.
+
+        player.localRotation = Quaternion.Slerp(player.localRotation, playerTargetRotation, smoothTime * Time.fixedDeltaTime);
+        camera.localRotation = Quaternion.Slerp(camera.localRotation, cameraTargetRotation, smoothTime * Time.fixedDeltaTime);
+
+        LockCursor();
+    }
+
+    // Lock the cursor to the center of the screen.
+    private void LockCursor() => Cursor.lockState = CursorLockMode.Locked;
+
+    // Method to clamp the camera rotation on the X Axis.
+    Quaternion ClampXRotation(Quaternion q) {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp (angleX, -90.0f, 90.0f);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+    
+}
