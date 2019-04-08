af3a000f66dde35bf0e2f7f9b3923ec322982a8e
Joel Filho <joel.landgraf@gmail.com>
2019-04-08 05:35:52 -0300

    playing around with object interations

---
+++ Assets/Scripts/InteractiveObjects.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObjects : MonoBehaviour
+{
+
+    [SerializeField] GameObject triggerObject;
+    [SerializeField] string triggerName;
+
+    ObjectActions objActions;
+    Animator openDoorAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objActions = triggerObject.GetComponent<ObjectActions>();
+        openDoorAnim = GetComponent<Animator>();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (triggerObject.name == triggerName && objActions.IsHolding)
+        {
+            openDoorAnim.SetTrigger("OpenDoor");
+        }
+    }
+}
--- Assets/Scripts/ObjectActions.cs
+++ Assets/Scripts/ObjectActions.cs
@@ -4,36 +4,42 @@ using UnityEngine.UI;
 public class ObjectActions : MonoBehaviour {
 
     [SerializeField] private float throwForce;
-    [SerializeField] Text objectText;
+    [SerializeField] private Text objectText;
+    [SerializeField] private GameObject crossHair;
     [SerializeField] Transform playerHand;
     [SerializeField] private bool isPickable;
+    [HideInInspector] public bool IsHolding { get { return isHolding; } }
 
     private Vector3 objectPos;
     private float distance;
     private Rigidbody objectRB;
+    private Image crossHairImage;
     private bool isHolding = false;
 
     private void Start() {
         objectRB = gameObject.GetComponent<Rigidbody>();
+        crossHairImage = crossHair.GetComponent<Image>();
     }
 
     // Shows up object name when mouse over it.
     private void OnMouseEnter() {
         objectText.text = gameObject.name;
         objectText.enabled = true;
+        crossHairImage.enabled = true;
     }
 
     // Clear the text.
     private void OnMouseExit() {
         objectText.text = "";
         objectText.enabled = false;
+        crossHairImage.enabled = false;
     }
 
     private void Update() {
 
         // Checks for the distance so the player only pick up objects that are near.
         distance = Vector3.Distance(gameObject.transform.position, playerHand.transform.position);
-        if (distance >= 1.0f) isHolding = false;
+        if (distance >= 1.3f) isHolding = false;
 
         if (isHolding == true) {
             ObjectIsPicked();
@@ -42,13 +48,13 @@ public class ObjectActions : MonoBehaviour {
                 ThrowObject();
             }
 
-        } else
-        {
+        } else {
             ObjectIsDropped();
         }
     }
 
     private void OnMouseUpAsButton() {
+        Debug.Log(distance);
         if (isPickable) {
             if (!isHolding) {
                 CanPickObject();
@@ -57,7 +63,7 @@ public class ObjectActions : MonoBehaviour {
     }
 
     private void CanPickObject() {
-        if (distance <= 1.0f) {
+        if (distance <= 1.3f) {
             isHolding = true;
             objectRB.useGravity = false;
             objectRB.detectCollisions = true;
@@ -67,11 +73,12 @@ public class ObjectActions : MonoBehaviour {
     private void ObjectIsPicked() {
         objectRB.velocity = Vector3.zero;
         objectRB.angularVelocity = Vector3.zero;
+        gameObject.transform.position = playerHand.transform.position;
+        gameObject.transform.rotation = playerHand.transform.rotation;
         gameObject.transform.SetParent(playerHand.transform);
     }
 
-    private void ObjectIsDropped()
-    {
+    private void ObjectIsDropped() {
         objectPos = gameObject.transform.position;
         gameObject.transform.SetParent(null);
         objectRB.useGravity = true;
